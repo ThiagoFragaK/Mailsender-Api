a60102aaf7224a4dde1ad89e6f120da4cdab0984
ThiagoFragaK <thiago@fragainformatica.com>
2024-06-24 22:47:28 -0300

    Add: Topic seeder

---
+++ database/seeders/TopicsSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Topics;
+
+class TopicsSeeder extends Seeder
+{
+    public function run(): Void
+    {
+        Topics::create([
+            
+        ]);
+    }
+}
