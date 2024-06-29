e56f1d8d258572752405aba1c6116b1c7eb2e078
ThiagoFragaK <thiagofraga@proton.me>
2024-06-29 10:45:10 -0300

    Add: Savemail method

--- app/Services/EmailsService.php
+++ app/Services/EmailsService.php
@@ -2,9 +2,11 @@
 
 namespace App\Services;
 
+use App\Mail\ActiveMail;
 use Exception;
 use App\Helpers\DateTimeHelper;
 use App\Models\Emails;
+use Illuminate\Support\Facades\Mail;
 class EmailsService
 {
     public function getEmailsByTopic(Int $topicID): Array
@@ -12,8 +14,22 @@ class EmailsService
         return [];
     }
 
-    public function dispatchEmail(Array $email): Bool
+    public function saveAndDispatchEmail(Array $email): Bool
     {
+        // $this->saveEmail($email);
+        Mail::to(env("MAIL_HOST"), "Thiago")->send(new ActiveMail($email));
         return true;
+    }
+
+    public function saveEmail(Array $email): Void
+    {
+        Emails::create([
+            "code" => rand(),
+            "sender" => $email["sender"] ?? "sender@mail.com",
+            "receiver" => $email["receiver"] ?? "receiver@mail.com",
+            "status" => 0,
+            "body" => $email["body"],
+            "created_at" => now(),
+        ]);
     }
 }
